ddbaf625ea38bbcd44752d703212ab949532c19f
Abdalla Sameh <abdallasameh13@gmail.com>
2025-12-23 22:00:29 +0200

    finally

---
+++ src/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Use JUnit in RunAll.java to run the tests.");
+        System.out.println("If that does not work run TestRunner.java");
+    }
+}
