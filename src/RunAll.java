04d3f30b108f32fa0538223d7222fbc9c4b24163
Abdalla Sameh <abdallasameh13@gmail.com>
2025-12-23 21:44:58 +0200

    junits

---
+++ src/RunAll.java
@@ -0,0 +1,13 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    SJFTest.class,
+    RRTest.class,
+    PriorityTest.class,
+    AGTest.class,
+})
+public class RunAll {
+    
+}
