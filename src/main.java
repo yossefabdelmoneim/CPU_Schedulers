5ed007b08300b5332d9fc0fd03a2080198c29175
Abdalla Sameh <abdallasameh13@gmail.com>
2025-12-23 21:53:19 +0200

    fix

--- src/main.java
+++
@@ -1,8 +0,0 @@
-
-public class main {
-    public static void main(String[] args) {
-        System.out.println("To run tests, please use the RunnAll.java file");
-        System.out.println("If that's not available, run the tests using TestRunner.java");
-        
-    }
-}
